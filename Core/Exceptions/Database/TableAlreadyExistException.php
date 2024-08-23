7b753232bc84ebd74c1fc896232bf6e9540f6602
Ahmet ÇELİK <ahmet.celik@tripy.mobi>
2024-08-23 20:22:39 +0300

    first exception class added

--- Core/Exceptions/Database/TableAlreadyExistException.php
+++ Core/Exceptions/Database/TableAlreadyExistException.php
@@ -6,17 +6,13 @@ use Exception;
 
 class TableAlreadyExistException extends Exception
 {
-    // Özel mesaj ve kod ile exception fırlatmak için constructor ekleyelim
     public function __construct($tableName, $code = 0, Exception $previous = null)
     {
-        // Özel mesaj: tablo adını mesajda belirtmek için özelleştiriyoruz
         $message = "The table '{$tableName}' already exists in the database.";
 
-        // Parent class'ın constructor'ını çağırıyoruz
         parent::__construct($message, $code, $previous);
     }
 
-    // İsteğe bağlı olarak, özel loglama veya hata işleme yöntemleri ekleyebilirsin
     public function __toString()
     {
         return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
